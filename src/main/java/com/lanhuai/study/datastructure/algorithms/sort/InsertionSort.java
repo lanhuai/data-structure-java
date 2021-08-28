6a0fc21a996b38bb03315f8d33661bf1ba9dd9b1
lanhuai <274324+lanhuai@users.noreply.github.com>
2021-08-29 00:09:32 +0800

    extract greaterThan method

--- src/main/java/com/lanhuai/study/datastructure/algorithms/sort/InsertionSort.java
+++ src/main/java/com/lanhuai/study/datastructure/algorithms/sort/InsertionSort.java
@@ -3,7 +3,7 @@ package com.lanhuai.study.datastructure.algorithms.sort;
 /**
  * @author lanhuai
  */
-public class InsertionSort<T> {
+public class InsertionSort<T extends Comparable<?>> {
     private Comparable<T>[] elements;
 
     public InsertionSort(Comparable<T>[] elements) {
@@ -16,31 +16,35 @@ public class InsertionSort<T> {
         // sort asc
         if (!desc) {
             for (int j = 1; j < elements.length; j++) {
-                Comparable<T> element = elements[j];
+                Comparable<T> key = elements[j];
 
                 // insert elements[j] into the sorted sequence elements[0...j - 1]
                 int i = j - 1;
-                while (i >= 0 && elements[i].compareTo((T) element) > 0) {
+                while (i >= 0 && greaterThan(elements[i], (T) key)) {
                     elements[i + 1] = elements[i];
                     i = i - 1;
                 }
-                elements[i + 1] = element;
+                elements[i + 1] = key;
             }
         }
 
         // sort desc
         if (desc) {
             for (int j = elements.length - 2; j >= 0; j--) {
-                Comparable<T> element = elements[j];
+                Comparable<T> key = elements[j];
 
                 // insert elements[j] into the sorted sequence elements[j + 1...n - 1]
                 int i = j + 1;
-                while (i < elements.length && elements[i].compareTo((T) element) > 0) {
+                while (i < elements.length && greaterThan(elements[i], (T) key)) {
                     elements[i - 1] = elements[i];
                     i = i + 1;
                 }
-                elements[i - 1] = element;
+                elements[i - 1] = key;
             }
         }
+    }
+
+    private boolean greaterThan(Comparable<T> a, T b) {
+        return a.compareTo(b) > 0;
     }
 }
